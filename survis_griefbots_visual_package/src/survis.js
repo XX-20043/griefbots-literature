b0b9f7a4285773d82a8c06d01618b5e324248c01
XX-20043 <songmu20043@yeah.net>
2025-05-16 10:49:07 +0100

    Add files via upload

---
+++ survis_griefbots_visual_package/src/survis.js
@@ -0,0 +1,7 @@
+window.survis = {
+    loadData: function (url) {
+        document.getElementById("vis").innerHTML = 
+            '<p style="color:green;"><b>Data loaded from: ' + url + '</b></p>' +
+            '<p>This is a placeholder for a fully interactive SurVis system.</p>';
+    }
+};
